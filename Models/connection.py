509eb9c7399ed428bdb23c57181fde3e4ca2d1eb
THE-MAIN39 <meharumerzero07@gmail.com>
2025-06-22 14:06:41 +0500

    connection.py

--- Models/connection.py
+++ Models/connection.py
@@ -1,11 +1,15 @@
 import mysql.connector
 
 def get_connection():
-    conn = mysql.connector.connect(
-        host="bsgpfm7hjvrfuuctqhaa-mysql.services.clever-cloud.com",
-        user="u25tcgnb0dpj2a3t",
-        password="jtQuCd4lNzhLl3qirC4e",
-        database="bsgpfm7hjvrfuuctqhaa",
-        port=3306
-    )
-    return conn
+    try:
+        conn = mysql.connector.connect(
+            host="bsgpfm7hjvrfuuctqhaa-mysql.services.clever-cloud.com",
+            user="u25tcgnb0dpj2a3t",
+            password="jtQuCd4lNzhLl3qirC4e",
+            database="bsgpfm7hjvrfuuctqhaa",
+            port=3306
+        )
+        return conn
+    except mysql.connector.Error as err:
+        print(f"❌ DB Connection Error: {err}")
+        return None
