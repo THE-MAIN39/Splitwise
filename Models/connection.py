8e71678b3609cfefa5db350e990c08e126cfc492
THE-MAIN39 <meharumerzero07@gmail.com>
2025-06-22 13:48:39 +0500

    connection.py

--- Models/connection.py
+++ Models/connection.py
@@ -1,12 +1,7 @@
 import mysql.connector
 
 def get_connection():
-    conn = mysql.connector.connect(
-        host="bsgpfm7hjvrfuuctqhaa-mysql.services.clever-cloud.com",
-        user="u25tcgnb0dpj2a3t",
-        password="jtQuCd4lNzhLl3qirC4e",
-        database="bsgpfm7hjvrfuuctqhaa",
-        port=3306,
+    
         ssl_disabled=False,  # ✅ Required for Clever Cloud
         ssl_verify_cert=False  # ✅ Since they disabled SSL verification
     )
